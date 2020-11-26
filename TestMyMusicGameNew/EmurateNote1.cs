3418d9a6432e3b04963afb3f6dc209b9e1a6933e
FourHyphen <FourHyphen.4@gmail.com>
2020-11-27 08:25:09 +0900

    FeatureテストのためにNote挙動エミュレート処理を共通化

---
+++ TestMyMusicGameNew/EmurateNote1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MyMusicGameNew;
+
+namespace TestMyMusicGameNew
+{
+    public class EmurateNote1
+    {
+        private static readonly int PlayAreaX = 800;
+        private static readonly int PlayAreaY = 600;
+        private static readonly double NoteSpeedYPerSec = 300.0;
+        private static readonly int XLine = 1;
+        public static TimeSpan JustTiming { get; private set; } = new TimeSpan(0, 0, 0, 1, 0);  // 1[s]
+
+        private static readonly double NotInsidePlayAreaWhenMusicStartJustTiming = PlayAreaY * 2;
+
+        public Note Note { get; private set; }
+
+        public double NowX { get { return Note.NowX; } }
+
+        public double NowY { get { return Note.NowY; } }
+
+        public EmurateNote1()
+        {
+            Init();
+        }
+
+        private void Init()
+        {
+            NoteData nd = new NoteData(XLine, JustTiming);
+            Note = new Note(nd, PlayAreaX, PlayAreaY, NoteSpeedYPerSec);
+        }
+
+        public void SetNowPoint(TimeSpan time)
+        {
+            Note.CalcNowPoint(time);
+        }
+
+        public System.Windows.Point EmurateCalcPoint(TimeSpan elapsedTimeFromGameStart)
+        {
+            double note1XPoint = XLine * (int)((double)PlayAreaX * 0.33333);
+            double diff = JustTiming.Subtract(elapsedTimeFromGameStart).TotalMilliseconds;
+            double note1YPoint = Note.JudgeLineYFromAreaTop - (diff * NoteSpeedYPerSec / 1000);
+            return new System.Windows.Point(note1XPoint, note1YPoint);
+        }
+
+        public System.Windows.Point EmurateCalcJustTimingPoint()
+        {
+            double note1XPoint = XLine * (int)((double)PlayAreaX * 0.33333);
+            double note1YPoint = Note.JudgeLineYFromAreaTop;
+            return new System.Windows.Point(note1XPoint, note1YPoint);
+        }
+
+        public bool IsInsidePlayArea()
+        {
+            return Note.IsInsidePlayArea();
+        }
+
+        public static Note GetNoteNotInsidePlayAreaWhenMusicStartJustTiming()
+        {
+            NoteData nd = new NoteData(XLine, JustTiming);
+            return new Note(nd, PlayAreaX, PlayAreaY, NotInsidePlayAreaWhenMusicStartJustTiming);
+        }
+    }
+}
--- TestMyMusicGameNew/TestNote.cs
+++ TestMyMusicGameNew/TestNote.cs
@@ -9,50 +9,37 @@ namespace TestMyMusicGameNew
     [TestClass]
     public class TestNote
     {
-        private static readonly int PlayAreaX = 800;
-        private static readonly int PlayAreaY = 600;
-        private static readonly double NoteSpeedYPerSec = 300.0;
-        private static readonly int Note1XLine = 1;
-        private static readonly TimeSpan Note1Time = new TimeSpan(0, 0, 0, 1, 0);  // 1[s]
         private static readonly TimeSpan ZeroTime = new TimeSpan(0, 0, 0, 0, 0);
-
-        private static readonly double NotInsidePlayAreaWhenMusicStartJustTiming = PlayAreaY * 2;
 
         [TestMethod]
         public void TestCorrectNoteCoordinateAtTheTime()
         {
-            NoteData nd = new NoteData(Note1XLine, Note1Time);
-            Note note = new Note(nd, PlayAreaX, PlayAreaY, NoteSpeedYPerSec);
+            EmurateNote1 emurateNote1 = new EmurateNote1();
 
             // 初期座標の確認
-            note.CalcNowPoint(ZeroTime);
-
-            double note1XPoint = Note1XLine * (int)((double)PlayAreaX * 0.33333);
-            double note1YPoint = note.JudgeLineYFromAreaTop - (Note1Time.TotalSeconds * NoteSpeedYPerSec);
-            Assert.AreEqual(expected: note1XPoint, actual: note.NowX);
-            Assert.AreEqual(expected: note1YPoint, actual: note.NowY);
+            emurateNote1.SetNowPoint(ZeroTime);
+            System.Windows.Point initPoint = emurateNote1.EmurateCalcPoint(ZeroTime);
+            Assert.AreEqual(expected: initPoint.X, actual: emurateNote1.NowX);
+            Assert.AreEqual(expected: initPoint.Y, actual: emurateNote1.NowY);
 
             // 判定線とちょうど重なるときの確認
-            note.CalcNowPoint(Note1Time);
+            emurateNote1.SetNowPoint(EmurateNote1.JustTiming);
+            System.Windows.Point justTimingPoint = emurateNote1.EmurateCalcJustTimingPoint();
 
-            double note1XPointJustTiming = note1XPoint;
-            double note1YPointJustTiming = note.JudgeLineYFromAreaTop;
-            Assert.AreEqual(expected: note1XPointJustTiming, actual: note.NowX);
-            Assert.AreEqual(expected: note1YPointJustTiming, actual: note.NowY);
+            Assert.AreEqual(expected: justTimingPoint.X, actual: emurateNote1.NowX);
+            Assert.AreEqual(expected: justTimingPoint.Y, actual: emurateNote1.NowY);
         }
 
         [TestMethod]
         public void TestCorrectWhetherInsidePlayArea()
         {
-            NoteData nd = new NoteData(Note1XLine, Note1Time);
-
-            Note noteOutside = new Note(nd, PlayAreaX, PlayAreaY, NotInsidePlayAreaWhenMusicStartJustTiming);
+            Note noteOutside = EmurateNote1.GetNoteNotInsidePlayAreaWhenMusicStartJustTiming();
             noteOutside.CalcNowPoint(ZeroTime);
             Assert.IsFalse(noteOutside.IsInsidePlayArea());
 
-            Note noteInside = new Note(nd, PlayAreaX, PlayAreaY, NoteSpeedYPerSec);
-            noteInside.CalcNowPoint(ZeroTime);
-            Assert.IsTrue(noteInside.IsInsidePlayArea());
+            EmurateNote1 emurateNote1 = new EmurateNote1();
+            emurateNote1.SetNowPoint(ZeroTime);
+            Assert.IsTrue(emurateNote1.IsInsidePlayArea());
         }
     }
 }
